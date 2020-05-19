0b5cbbfa0c30f7464e9bad75e9002fc790bcbdd5
Jussi H <45433002+jussihayha@users.noreply.github.com>
2020-05-19 20:01:18 +0300

    Added always awake for testing

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import * as React from "react";
 import AuthNavigator from "./navigation/AuthNavigator";
+import { useKeepAwake } from 'expo-keep-awake';
+
 console.disableYellowBox = true;
 
 export default function App() {
+
+  // used for testing purposes
+  useKeepAwake();
   return <AuthNavigator />;
 }
