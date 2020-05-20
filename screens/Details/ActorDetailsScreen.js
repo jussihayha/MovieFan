60a84e1f9d09105f23e35149a38669b9ab90825e
Jussi H <45433002+jussihayha@users.noreply.github.com>
2020-05-20 13:41:15 +0300

    Multiple iterations
    I seem to have an issue with images.
    Either I dont get images or the list looks ugly.

--- screens/Details/ActorDetailsScreen.js
+++ screens/Details/ActorDetailsScreen.js
@@ -1,18 +1,19 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
-import { View, StyleSheet, ScrollView, Image, Dimensions } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import { Text, ListItem } from "react-native-elements";
 import { MOVIE_KEY } from "react-native-dotenv";
-
+import { en, fi } from "../../components/lang/Translations";
+import i18n from "i18n-js";
 export default function ActorDetailsScreen({ route, navigation }) {
-  const { actor, profile } = route.params;
-  const { known } = route.params;
+  const { actor } = route.params;
+
   const [actorDetails, setActorDetails] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchDetails = async () => {
-      const url = `https://api.themoviedb.org/3/person/${actor}/movie_credits?api_key=${MOVIE_KEY}&language=en-US&sort_by=release_date.asc`;
+      const url = `https://api.themoviedb.org/3/person/${actor}/movie_credits?api_key=${MOVIE_KEY}&language=en-US&page=2`;
       let response = await fetch(url);
       response = await response.json();
 
@@ -20,55 +21,36 @@ export default function ActorDetailsScreen({ route, navigation }) {
       setLoading(false);
     };
     fetchDetails();
-
   }, []);
 
-
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <ScrollView>
-          <Image
-            resizeMode="contain"
-            source={{
-              uri: `http://image.tmdb.org/t/p/original${profile}`,
-              width: Dimensions.get("window").width,
-              height: 600,
-            }}
-          />
-          <Text style={styles.paragraph}>Biography:</Text>
-        </ScrollView>
-
-        <Text style={styles.header}> Popular actors at the moment </Text>
+    <View style={{ backgroundColor: "#282D4F" }}>
+      <Text style={styles.header}>{i18n.t("popular_movies")}</Text>
+      <View style={styles.cards}>
         <ScrollView>
           {loading
             ? null
             : actorDetails.map((movie, index) => {
-              return (
-                <ListItem
-                  key={index}
-                  title={movie.title}
-                  leftAvatar={{
-                    source: {
-                      uri: `http://image.tmdb.org/t/p/original${movie.poster_path}`,
-                    },
-                  }}
-                  rightSubtitle={
-                    movie.vote_average == "0,"
-                      ? null
-                      : `Rating: ${movie.vote_average}`
-                  }
-                  rightTitle={movie.release_date}
+                return (
+                  <ListItem
+                    key={index}
+                    title={movie.title}
+                    leftAvatar={{
+                      source: {
+                        uri: `http://image.tmdb.org/t/p/original${movie.poster_path}`,
+                      },
+                    }}
+                    rightSubtitle={movie.vote_average}
                     bottomDivider={true}
-                    onPress={() =>
-                      navigation.navigate("Movie details", { movie: movie })
-                    }
+                    onPress={() => {
+                      navigation.navigate("Movie details", { movie: movie });
+                    }}
                     onLongPress={() => console.log(movie)}
                   />
                 );
               })}
         </ScrollView>
-      </ScrollView>
+      </View>
     </View>
   );
 }
@@ -76,43 +58,40 @@ export default function ActorDetailsScreen({ route, navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000000",
-    padding: 8,
+    backgroundColor: "#282D4F",
   },
-  paragraph: {
-    margin: 24,
+
+  header: {
     fontSize: 24,
     color: "white",
     fontWeight: "bold",
-    textAlign: "center",
-    textDecorationLine: "underline",
-  },
-
-  bio: {
-    fontSize: 16,
-    color: "white",
+    marginTop: 20,
   },
 
   list: {
-    textAlign: "center",
-    fontSize: 18,
-    fontWeight: "bold",
-    justifyContent: "space-between",
     color: "white",
-    marginBottom: 40,
   },
 
-  title: {
-    fontSize: 36,
-    justifyContent: "center",
-    backgroundColor: "#000",
-    color: "white",
+  images: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
   },
 
   movies: {
     width: 300,
-    height: 400,
+    height: 300,
+    backgroundColor: "#000",
   },
+
+  title: {
+    fontSize: 24,
+    justifyContent: "center",
+    backgroundColor: "#000",
+    color: "white",
+    alignSelf: "center",
+  },
+
   cards: {
     borderWidth: 0,
     backgroundColor: "black",
