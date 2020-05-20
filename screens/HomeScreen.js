273aa3badc595ae7dae1d6d248bf3ca30b66893a
Jussi H <45433002+jussihayha@users.noreply.github.com>
2020-05-20 13:41:34 +0300

    Switched colors

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -61,7 +61,7 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000000",
+    backgroundColor: "#282D4F",
   },
 
   header: {
